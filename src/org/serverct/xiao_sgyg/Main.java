872cedfe6127e8409baba6a18de4d2fb26116c96
LovesAsuna <625924077@qq.com>
2019-10-05 22:53:06 +0800

    增加版本检测

--- src/org/serverct/xiao_sgyg/Main.java
+++ src/org/serverct/xiao_sgyg/Main.java
@@ -6,6 +6,11 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.serverct.xiao_sgyg.command.POrderCommand;
 import org.serverct.xiao_sgyg.listener.*;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+
 
 public class Main extends JavaPlugin{
 	
@@ -14,7 +19,12 @@ public class Main extends JavaPlugin{
 	public void onEnable() {
 		plugin = this;
 		saveDefaultConfig();
-		getServer().getConsoleSender().sendMessage("[POrder] Plugin Enable");
+		try {
+			Version();
+		} catch (IOException e) {
+			getLogger().severe("An error occurres when check the plugin version!");
+			e.printStackTrace();
+		}
 		Bukkit.getPluginCommand("PO").setExecutor((CommandExecutor) new POrderCommand());
 		Bukkit.getPluginManager().registerEvents(new POrderEnchant(), this);
 		Bukkit.getPluginManager().registerEvents(new POrderCraft(), this);
@@ -45,7 +55,30 @@ public class Main extends JavaPlugin{
     }
 	
 	public void onDisable() {
-		
 		getServer().getConsoleSender().sendMessage("[POrder] Plugin Disenable");
+	}
+
+	public String getNewestVersion() throws IOException {
+		URL url = new URL("https://api.github.com/repos/LovesAsuna/ProhibitionOrder/releases");
+		BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
+		String version = reader.readLine().split(",") [7];
+		String[] replace = {"tag_name","\"",":"};
+		for (String cr :replace) {
+			version = version.replace(cr,"");
+		}
+		reader.close();
+		return version;
+	}
+
+	public void Version() throws IOException {
+		String currentVersion = getDescription().getVersion();
+		String newVersion = getNewestVersion();
+		getServer().getConsoleSender().sendMessage("¡ì7[POrder] Plugin Enable");
+		if (currentVersion.equalsIgnoreCase(newVersion)) {
+			getServer().getConsoleSender().sendMessage("¡ì7[POrder]¡ì2You are using the newest " + currentVersion + "version");
+		} else {
+			getServer().getConsoleSender().sendMessage("¡ì7[POrder]¡ìcThe newest version is " + newVersion);
+			getServer().getConsoleSender().sendMessage("¡ì7[POrder]¡ìcPlease download on https://www.spigotmc.org/resources/porder.71367/");
+		}
 	}
 }
