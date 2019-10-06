feaa3af8f7f0ab2609f9dab2af4197d171385a53
LovesAsuna <625924077@qq.com>
2019-10-06 09:22:24 +0800

    异步处理版本检测

--- src/org/serverct/xiao_sgyg/Main.java
+++ src/org/serverct/xiao_sgyg/Main.java
@@ -2,6 +2,7 @@ package org.serverct.xiao_sgyg;
 
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandExecutor;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.serverct.xiao_sgyg.command.POrderCommand;
 import org.serverct.xiao_sgyg.listener.*;
@@ -19,30 +20,16 @@ public class Main extends JavaPlugin{
 	public void onEnable() {
 		plugin = this;
 		saveDefaultConfig();
-		try {
-			Version();
-		} catch (IOException e) {
-			getLogger().severe("An error occurres when check the plugin version!");
-			e.printStackTrace();
+		getServer().getConsoleSender().sendMessage("[POrder] Plugin Enable");
+		CheckRunnable checkVersion= new CheckRunnable();
+		Thread check = new Thread(checkVersion);
+		check.run();
+		Listener[] listeners = {new POrderEnchant(),new POrderCraft(),new POrderbrewing(),new POrderFurnace(),new POrderChat(),new POrderAnvil(),new POrderBreak(),
+				new POrderPlace(),new POrderEntity(),new POrderDrop(),new POrderFishing(),new POrderHold(),new POrderLightningStrike(),new POrderThunder(),
+				new POrderChange(),new InventoryClick(),new Debug(),new Redstone(),new Bancommand()};
+		for (Listener listener : listeners) {
+			Bukkit.getPluginManager().registerEvents(listener,this);
 		}
-		Bukkit.getPluginCommand("PO").setExecutor((CommandExecutor) new POrderCommand());
-		Bukkit.getPluginManager().registerEvents(new POrderEnchant(), this);
-		Bukkit.getPluginManager().registerEvents(new POrderCraft(), this);
-		Bukkit.getPluginManager().registerEvents(new POrderbrewing(), this);
-		Bukkit.getPluginManager().registerEvents(new POrderFurnace(), this);
-		Bukkit.getPluginManager().registerEvents(new POrderChat(), this);
-		Bukkit.getPluginManager().registerEvents(new POrderAnvil(), this);
-		Bukkit.getPluginManager().registerEvents(new POrderBreak(), this);
-		Bukkit.getPluginManager().registerEvents(new POrderPlace(), this);
-		Bukkit.getPluginManager().registerEvents(new POrderEntity(), this);
-		Bukkit.getPluginManager().registerEvents(new POrderDrop(), this);
-		Bukkit.getPluginManager().registerEvents(new POrderFishing(), this);
-		Bukkit.getPluginManager().registerEvents(new POrderHold(), this);
-		Bukkit.getPluginManager().registerEvents(new POrderLightningStrike(), this);
-		Bukkit.getPluginManager().registerEvents(new POrderThunder(), this);
-		Bukkit.getPluginManager().registerEvents(new POrderChange(), this);
-		Bukkit.getPluginManager().registerEvents(new InventoryClick(),this);
-		Bukkit.getPluginManager().registerEvents(new Debug(),this);
 		reloadConfig();
 		}
 	
@@ -58,27 +45,29 @@ public class Main extends JavaPlugin{
 		getServer().getConsoleSender().sendMessage("[POrder] Plugin Disenable");
 	}
 
-	public String getNewestVersion() throws IOException {
-		URL url = new URL("https://api.github.com/repos/LovesAsuna/ProhibitionOrder/releases");
-		BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
-		String version = reader.readLine().split(",") [7];
-		String[] replace = {"tag_name","\"",":"};
-		for (String cr :replace) {
-			version = version.replace(cr,"");
-		}
-		reader.close();
-		return version;
-	}
-
-	public void Version() throws IOException {
-		String currentVersion = getDescription().getVersion();
-		String newVersion = getNewestVersion();
-		getServer().getConsoleSender().sendMessage("¡ì7[POrder] Plugin Enable");
-		if (currentVersion.equalsIgnoreCase(newVersion)) {
-			getServer().getConsoleSender().sendMessage("¡ì7[POrder]¡ì2You are using the newest " + currentVersion + " version");
-		} else {
-			getServer().getConsoleSender().sendMessage("¡ì7[POrder]¡ìcThe newest version is " + newVersion);
-			getServer().getConsoleSender().sendMessage("¡ì7[POrder]¡ìcPlease download on https://www.spigotmc.org/resources/porder.71367/");
+	class CheckRunnable implements Runnable {
+		@Override
+		public void run() {
+			try {
+				URL url = new URL("https://api.github.com/repos/LovesAsuna/ProhibitionOrder/releases");
+				BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
+				String newestversion = reader.readLine().split(",") [7];
+				String currentVersion = getDescription().getVersion();
+				String[] replace = {"tag_name","\"",":"};
+				for (String cr :replace) {
+					newestversion = newestversion.replace(cr,"");
+				}
+				reader.close();
+				if (currentVersion.equalsIgnoreCase(newestversion)) {
+					getServer().getConsoleSender().sendMessage("¡ì7[POrder]¡ì2You are using the newest " + currentVersion + " version");
+				} else {
+					getServer().getConsoleSender().sendMessage("¡ì7[POrder]¡ìcThe newest version is " + newestversion);
+					getServer().getConsoleSender().sendMessage("¡ì7[POrder]¡ìcPlease download on https://www.spigotmc.org/resources/porder.71367/");
+				}
+			} catch (IOException e) {
+				getLogger().severe("An error occurres when check the plugin version!");
+				e.printStackTrace();
+			}
 		}
 	}
 }
